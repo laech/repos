060c95e9d5322ad1bef7c9cf7da11ef8c58c243d
Lae <laec@protonmail.ch>
2017-12-02 19:42:52 +1300

    Update name.

--- src/Project/Gitlab.hs
+++ src/Project/Gitlab.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Project.Gitlab
@@ -7,21 +7,22 @@ module Project.Gitlab
 
 import qualified Data.ByteString.Lazy.Char8 as LazyChar8
 
-import           Control.Exception
-import           Data.Aeson
-import           Data.CaseInsensitive
-import           Data.Typeable
-import           GHC.Generics
-import           Network.HTTP.Client
-import           Network.HTTP.Client.TLS
-import           Network.HTTP.Types.Header
-import           Project.Exception
+import Control.Exception
+import Data.Aeson
+import Data.CaseInsensitive
+import Data.Typeable
+import GHC.Generics
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import Network.HTTP.Types.Header
+import Project.Exception
 
 newtype Repo = Repo
-  { ssh_url_to_repo :: String
+  { sshUrl :: String
   } deriving (Show, Generic)
 
-instance FromJSON Repo
+instance FromJSON Repo where
+  parseJSON = withObject "Repo" $ \v -> Repo <$> v .: "ssh_url_to_repo"
 
 type Token = String
 
@@ -32,7 +33,7 @@ getGitlabRepoSshUrls token = do
   let url =
         "https://gitlab.com/api/v4/projects?owned=true&private_token=" ++ token
   manager <- newManager tlsManagerSettings
-  fmap ssh_url_to_repo <$> getRepos url manager
+  fmap sshUrl <$> getRepos url manager
 
 getRepos :: Url -> Manager -> IO [Repo]
 getRepos url manager = do
@@ -40,10 +41,9 @@ getRepos url manager = do
   response <- httpLbs request manager
   case parseRepos . responseBody $ response of
     Right repos -> return repos
-    Left err    -> throwIO $ JsonException err
+    Left err -> throwIO $ JsonException err
   where
-    withHeaders request = request
-      {requestHeaders = [(hUserAgent, "None")]}
+    withHeaders request = request {requestHeaders = [(hUserAgent, "None")]}
 
 parseRepos :: LazyChar8.ByteString -> Either String [Repo]
 parseRepos = eitherDecode
