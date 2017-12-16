bfc615d335b8cac18cef5bc6dc5c8df9ac50990c
Lae <laec@protonmail.ch>
2017-12-17 11:31:41 +1300

    Use output-concurrent.

--- src/Project/Git.hs
+++ src/Project/Git.hs
@@ -1,16 +1,10 @@
 module Project.Git
-  ( fetchRepos
+  ( fetchRepo
   ) where
 
-import Control.Concurrent.Async
-import Control.Concurrent.Chan
-import Control.Exception
-import Control.Monad
-import Data.List
 import System.Directory
 import System.Exit
 import System.FilePath
-import System.IO
 import System.Process
 
 getProjectNameFromSshUrl :: String -> String
@@ -34,60 +28,10 @@ gitClone :: String -> FilePath -> CreateProcess
 gitClone sshUrl parentDir =
   (shell $ "git clone --quiet " ++ sshUrl) {cwd = Just parentDir}
 
-getStatusSymbol :: ExitCode -> String
-getStatusSymbol exitCode =
-  case exitCode of
-    ExitSuccess -> "✓"
-    ExitFailure _ -> "✗"
-
-getOutputHandle :: ExitCode -> Handle
-getOutputHandle exitCode =
-  case exitCode of
-    ExitSuccess -> stdout
-    ExitFailure _ -> stderr
-
-type IOChan = Chan (Maybe (IO ()))
-
-fetchRepo :: IOChan -> FilePath -> String -> IO ExitCode
-fetchRepo output parentDir sshUrl = do
-  result@(exitCode, out, err) <- doGitUpdate parentDir sshUrl
-  writeChan output $ Just (printGitResult result sshUrl)
-  return exitCode
-
-doGitUpdate :: FilePath -> String -> IO (ExitCode, String, String)
-doGitUpdate parentDir sshUrl = do
+fetchRepo :: FilePath -> String -> IO (ExitCode, String, String)
+fetchRepo parentDir sshUrl = do
   let projectDir = parentDir </> getProjectNameFromSshUrl sshUrl
   exists <- doesDirectoryExist projectDir
   if exists
     then execute [gitFetch projectDir, gitMerge projectDir]
     else readCreateProcessWithExitCode (gitClone sshUrl parentDir) ""
-
-printGitResult :: (ExitCode, String, String) -> String -> IO ()
-printGitResult (exitCode, out, err) sshUrl = do
-  hPutStr handle $ status ++ " " ++ sshUrl ++ "\n" ++ message exitCode
-  hFlush handle
-  where
-    handle = getOutputHandle exitCode
-    status = getStatusSymbol exitCode
-    message exitCode =
-      if exitCode == ExitSuccess
-        then ""
-        else intercalate "\n" (filter (not . null) [out, err])
-
-run :: IOChan -> IO ()
-run chan = do
-  maybeAction <- readChan chan
-  case maybeAction of
-    Nothing -> return ()
-    Just action -> do
-      action
-      run chan
-
-fetchRepos :: FilePath -> [String] -> IO ()
-fetchRepos parentDir sshUrls = do
-  output <- newChan :: IO IOChan
-  outputer <- async (run output)
-  results <- mapConcurrently (fetchRepo output parentDir) sshUrls
-  writeChan output Nothing
-  wait outputer
-  when (any (/= ExitSuccess) results) $ throwIO $ ExitFailure 1
--- src/Project/Main.hs
+++ src/Project/Main.hs
@@ -2,22 +2,27 @@ module Main where
 
 import Control.Concurrent.Async
 import Control.Exception
+import Control.Monad
 import Data.Either
+import Data.List
 import Network.HTTP.Client.TLS
 import Project.Bitbucket
 import Project.Config
 import Project.Git
 import Project.Gitlab
+import System.Console.Concurrent
+import System.Console.Regions
 import System.Environment
 import System.Exit
 import System.IO
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [configFile] -> process configFile >>= exitWith
-    _ -> die "Usage: <this-program> <config-file>"
+main =
+  displayConsoleRegions $ do
+    args <- getArgs
+    case args of
+      [configFile] -> process configFile >>= exitWith
+      _ -> die "Usage: <this-program> <config-file>"
 
 process :: FilePath -> IO ExitCode
 process path = do
@@ -27,8 +32,10 @@ process path = do
   results <-
     mapConcurrently
       (processSshUrls (directory config))
-      [ getGitlabRepoSshUrls manager (gitlabToken config)
-      , getBitbucketRepoSshUrls
+      [ info "> Getting Gitlab repositories..." $
+        getGitlabRepoSshUrls manager (gitlabToken config)
+      , info "> Getting Bitbucket repositories..." $
+        getBitbucketRepoSshUrls
           manager
           (bitbucketUsername config)
           (bitbucketPassword config)
@@ -38,10 +45,43 @@ process path = do
       then ExitSuccess
       else ExitFailure 1
 
+info :: String -> IO a -> IO a
+info msg io =
+  withConsoleRegion Linear $ \region -> setConsoleRegion region msg >> io
+
 processSshUrls :: FilePath -> IO [String] -> IO ExitCode
 processSshUrls directory sshUrls = do
   result <-
     try (sshUrls >>= fetchRepos directory) :: IO (Either SomeException ())
   case result of
-    Left e -> print e >> return (ExitFailure 1)
+    Left e -> outputConcurrent (show e ++ "\n") >> return (ExitFailure 1)
     _ -> return ExitSuccess
+
+fetchRepos :: FilePath -> [String] -> IO ()
+fetchRepos parentDir sshUrls = do
+  results <- mapConcurrently fetch sshUrls
+  when (any (/= ExitSuccess) results) $ throwIO $ ExitFailure 1
+  where
+    fetch :: String -> IO ExitCode
+    fetch sshUrl =
+      withConsoleRegion Linear $ \region -> do
+        setConsoleRegion region ("> " ++ sshUrl)
+        result@(exitCode, stdout, stderr) <- fetchRepo parentDir sshUrl
+        finishConsoleRegion region (formatResult sshUrl result)
+        return exitCode
+
+getStatusSymbol :: ExitCode -> String
+getStatusSymbol exitCode =
+  if exitCode == ExitSuccess
+    then "✓"
+    else "✗"
+
+formatResult :: String -> (ExitCode, String, String) -> String
+formatResult sshUrl (exitCode, out, err) =
+  status ++ " " ++ sshUrl ++ message exitCode
+  where
+    status = getStatusSymbol exitCode
+    message exitCode =
+      if exitCode == ExitSuccess
+        then ""
+        else "\n" ++ intercalate "\n" (filter (not . null) [out, err])
