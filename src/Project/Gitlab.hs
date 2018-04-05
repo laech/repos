cff9dff644424c5ca510435b20bf9bf30f09d079
Lae <laec@protonmail.ch>
2018-04-05 16:29:30 +1200

    Refactor.

--- src/Project/Gitlab.hs
+++ src/Project/Gitlab.hs
@@ -5,8 +5,7 @@ module Project.Gitlab
   ( getGitlabRepoSshUrls
   ) where
 
-import qualified Data.Aeson.Types as Json
-import qualified Data.ByteString.Char8 as Char8
+import qualified Data.ByteString.Char8 as C
 
 import Data.Aeson
 import GHC.Generics
@@ -28,12 +27,16 @@ getGitlabRepoSshUrls manager config =
 repos :: Manager -> String -> IO [Repo]
 repos manager token = do
   let url = "https://gitlab.com/api/v4/projects?owned=true"
+  content <- readContent manager url token
+  either fail pure (eitherDecode content)
+
+readContent manager url token = do
   debugM "Gitlab" (". " ++ url)
   request <- withToken token <$> parseUrlThrow url
   response <- httpLbs request manager
   infoM "Gitlab" ("✓ " ++ url)
-  either fail return (eitherDecode . responseBody $ response)
+  pure $ responseBody response
 
 withToken :: String -> Request -> Request
 withToken token request =
-  request {requestHeaders = [("Private-Token", Char8.pack token)]}
+  request {requestHeaders = [("Private-Token", C.pack token)]}
