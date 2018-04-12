54a94dcc37d882936a7575e4b0938bfee0ae5a53
Lae <laec@protonmail.ch>
2018-04-12 23:05:36 +1200

    Fixes.

--- src/Project/Bitbucket.hs
+++ src/Project/Bitbucket.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Project.Bitbucket
@@ -5,55 +6,73 @@ module Project.Bitbucket
   ) where
 
 import qualified Data.ByteString.Char8 as C
+import qualified Pipes.Parse as P
 
+import Control.Exception
 import Data.Aeson
 import Data.Aeson.Types
 import Data.HashMap.Strict ((!))
+import Data.Typeable
+import GHC.Generics
 import Network.HTTP.Client
 import Network.URI
 import Pipes
+import Pipes.Aeson as PA
+import Pipes.HTTP
 import Project.Config
 import System.Log.Logger
 
+data Page = Page
+  { sshUrls :: [String]
+  , nextUrl :: Maybe String
+  } deriving (Show, Eq, Generic)
+
+instance FromJSON Page where
+  parseJSON =
+    withObject "" $ \v -> do
+      next <- v .:? "next"
+      repos <- v .: "values" :: Parser [Object]
+      links <- mapM (.: "links") repos
+      clones <- concat <$> mapM (.: "clone") links
+      sshUrls <- mapM (.: "href") $ filter isSsh clones
+      pure $ Page sshUrls next
+    where
+      isSsh clone = clone ! "name" == "ssh"
+
+newtype BitbucketException =
+  BitbucketException String
+  deriving (Show, Typeable)
+
+instance Exception BitbucketException
+
 getBitbucketRepoSshUrls :: Manager -> Config -> Producer String IO ()
 getBitbucketRepoSshUrls manager config =
-  repos manager (bitbucketUsername config) (bitbucketPassword config) url
+  getRepos manager (bitbucketUsername config) (bitbucketPassword config) url
   where
     url = "https://bitbucket.org/api/2.0/repositories/" ++ path
     path = escapeURIString isAllowedInURI (bitbucketUsername config)
 
-repos :: Manager -> String -> String -> String -> Producer String IO ()
-repos manager user pass url = do
-  result <- lift $ readPage manager url user pass
-  (urls, nextUrl) <- either fail pure result
+getRepos :: Manager -> String -> String -> String -> Producer String IO ()
+getRepos manager user pass url = do
+  (Page urls nextUrl) <- lift $ getPage manager url user pass
   for (each urls) yield
   maybe (pure ()) next nextUrl
   where
-    next = repos manager user pass
+    next = getRepos manager user pass
 
-type Page = ([String], Maybe String)
-
-readPage :: Manager -> String -> String -> String -> IO (Either String Page)
-readPage manager url user pass = do
+getPage :: Manager -> String -> String -> String -> IO Page
+getPage manager url user pass = do
   debugM "Bitbucket" (". " ++ url)
   request <- basicAuthRequest url user pass
-  withResponse request manager (fmap parse . responseBody) <*
-    infoM "Bitbucket" ("✓ " ++ url)
+  withHTTP request manager $ \response -> do
+    result <- P.evalStateT PA.decode (responseBody response)
+    case result of
+      Nothing -> failed "end of input"
+      Just (Left e) -> failed (show e)
+      Just (Right page) -> page <$ infoM "Bitbucket" ("✓ " ++ url)
   where
-    parse content = eitherDecodeStrict content >>= parseEither parsePage
+    failed = throwIO . BitbucketException
 
 basicAuthRequest :: String -> String -> String -> IO Request
 basicAuthRequest url user pass =
   applyBasicAuth (C.pack user) (C.pack pass) <$> parseUrlThrow url
-
-parsePage :: Value -> Parser ([String], Maybe String)
-parsePage =
-  withObject "" $ \v -> do
-    next <- v .:? "next"
-    repos <- v .: "values" :: Parser [Object]
-    links <- mapM (.: "links") repos
-    clones <- concat <$> mapM (.: "clone") links
-    sshUrls <- mapM (.: "href") $ filter isSsh clones
-    pure (sshUrls, next)
-  where
-    isSsh clone = clone ! "name" == "ssh"
--- src/Project/Gitlab.hs
+++ src/Project/Gitlab.hs
@@ -6,42 +6,56 @@ module Project.Gitlab
   ) where
 
 import qualified Data.ByteString.Char8 as C
+import qualified Pipes.Aeson as PA
+import qualified Pipes.Prelude as P
 
+import Control.Exception
 import Control.Monad
 import Data.Aeson
+import Data.Typeable
 import GHC.Generics
 import Network.HTTP.Client
 import Pipes
+import Pipes.HTTP
+import Pipes.Parse
 import Project.Config
 import System.Log.Logger
 
 newtype Repo = Repo
   { sshUrl :: String
-  } deriving (Show, Generic)
+  } deriving (Show, Eq, Generic)
 
 instance FromJSON Repo where
   parseJSON = withObject "Repo" $ \v -> Repo <$> v .: "ssh_url_to_repo"
 
+newtype GitlabException =
+  GitlabException String
+  deriving (Show, Typeable)
+
+instance Exception GitlabException
+
 getGitlabRepoSshUrls :: Manager -> Config -> Producer String IO ()
 getGitlabRepoSshUrls manager config =
-  for (repos manager $ gitlabToken config) $ yield . sshUrl
+  getRepos manager (gitlabToken config) >-> P.map sshUrl
 
-repos :: Manager -> String -> Producer Repo IO ()
-repos manager token = do
+getRepos :: Manager -> String -> Producer Repo IO ()
+getRepos manager token = do
   let url = "https://gitlab.com/api/v4/projects?owned=true"
   lift $ debugM "Gitlab" (". " ++ url)
   request <- withToken token <$> parseUrlThrow url
-  repos' <- lift $ readContent manager url token
+  repos <- lift $ readContent manager url token
   lift $ infoM "Gitlab" ("✓ " ++ url)
-  for (each repos') yield
+  for (each repos) yield
 
 readContent :: Manager -> String -> String -> IO [Repo]
 readContent manager url token = do
   request <- withToken token <$> parseUrlThrow url
-  withResponse request manager $ \response -> do
-    content <- responseBody response
-    result <- either fail pure $ eitherDecodeStrict content
-    pure result
+  withHTTP request manager $ \response ->
+    evalStateT PA.decode (responseBody response) >>=
+    maybe (failed "end of input") pure >>=
+    either (failed . show) pure
+  where
+    failed = throwIO . GitlabException
 
 withToken :: String -> Request -> Request
 withToken token request =
